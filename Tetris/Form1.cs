99c75d075cfcfda0e0b2c4b7573879ec4fd7b461
InsarL <64440932+InsarL@users.noreply.github.com>
2020-06-06 18:47:37 +0300

    Сделана разметка игрового поля

---
+++ Tetris/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tetris
+{
+    public partial class Form1 : Form
+    {
+        private Game game;
+        public Form1()
+        {
+            InitializeComponent();
+
+            game = new Game();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            game.Draw(e.Graphics);
+        }
+    }
+}
---
+++ Tetris/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class Game
+    {
+        public int GameFieldSize = 10;
+        public int CellSize = 25;
+        public void Draw(Graphics graphiks)
+        {
+            for (int i = 0; i <= GameFieldSize; i++)
+            {
+                graphiks.DrawLine(Pens.Black, 0, i * CellSize, GameFieldSize*CellSize, i * CellSize);
+                graphiks.DrawLine(Pens.Black, 0, (GameFieldSize+ i) * CellSize,
+                                                 GameFieldSize * CellSize, (GameFieldSize+i) * CellSize);
+                graphiks.DrawLine(Pens.Black, i * CellSize, 0, i * CellSize, GameFieldSize * CellSize);
+                graphiks.DrawLine(Pens.Black, i*CellSize, GameFieldSize*CellSize,i*CellSize , 2*CellSize*GameFieldSize);
+
+            }
+            
+        }
+    }
+}
