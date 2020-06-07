2f51bc82bf14d7a949d0af2c8168b5603a796919
InsarL <64440932+InsarL@users.noreply.github.com>
2020-06-07 20:12:51 +0300

    Добавлено: Теперь будущие фигуры нормально падают на "дно стакана". Также исчезают линииприих заполнении.

--- Tetris/Game.cs
+++ Tetris/Game.cs
@@ -11,12 +11,11 @@ namespace Tetris
     class Game
     {
         public static int HorizontalSizePlayingField = 10;
-        public int VerticalSizePlayingField = 20;
+        public static int VerticalSizePlayingField = 20;
         public int CellSize = 25;
         public Point Square = new Point(HorizontalSizePlayingField / 2, -1);
         public Keys DirectionFigure;
-
-
+        public List <Point> BusyCell = new List<Point>();
 
         public void Draw(Graphics graphiks)
         {
@@ -29,6 +28,11 @@ namespace Tetris
             }
 
             graphiks.FillRectangle(Brushes.BlueViolet, Square.X * CellSize, Square.Y * CellSize, CellSize, CellSize);
+
+            foreach  (Point cell in BusyCell)
+            {
+                graphiks.FillRectangle(Brushes.BlueViolet, cell.X * CellSize, cell.Y * CellSize, CellSize, CellSize);
+            }
         }
 
         public void MovementFigure(Keys key)
@@ -39,13 +43,14 @@ namespace Tetris
                 key == Keys.Left)
                 DirectionFigure = key;
 
-            if (DirectionFigure == Keys.Left)
+            if (DirectionFigure == Keys.Left && IsPossibleMoveShapeInThisDirection( Square.X-1,Square.Y))
                 Square.X--;
 
-            if (DirectionFigure == Keys.Right)
+            if (DirectionFigure == Keys.Right && IsPossibleMoveShapeInThisDirection(Square.X + 1, Square.Y))
                 Square.X++;
 
-            if (DirectionFigure == Keys.Down)
+            if (DirectionFigure == Keys.Down
+                && IsPossibleMoveShapeInThisDirection(Square.X, Square.Y+1))
                 Square.Y++;
 
             if (DirectionFigure == Keys.Up)
@@ -54,12 +59,31 @@ namespace Tetris
             DirectionFigure = Keys.None;
         }
 
+        public bool IsPossibleMoveShapeInThisDirection(int x, int y)
+        {
+            return x < HorizontalSizePlayingField && y < VerticalSizePlayingField && x >= 0 && y >= 0 && !BusyCell.Contains(new Point (x,y));
+        }
+
         public void Update()
         {
-            if (Square.Y >= VerticalSizePlayingField-1)
+            if (Square.Y == VerticalSizePlayingField - 1 || BusyCell.Contains(new Point (Square.X, Square.Y+1)))
+            {
+                BusyCell.Add(new Point(Square.X, Square.Y));
                 Square.Y = -1;
+            }
 
             Square.Y++;
+
+            for (int i = 0; i < VerticalSizePlayingField; i++)
+            {
+                List<Point> cellsUp = new List<Point> ();
+                if (BusyCell.Count(x => x.Y == i) == HorizontalSizePlayingField)
+                {
+                    BusyCell.Where(x => x.Y < i).Select(point => new Point(point.X, point.Y + 1)).ToList();
+                    BusyCell.RemoveAll(x => x.Y <= i);
+                    BusyCell.AddRange(cellsUp);
+                }
+            }
         }
     }
 }
